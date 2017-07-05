9de6842cb264b3aaaa415b626798b55aeb154874
Jason Hannon <C00178466@itcarlow.ie>
2017-07-05 15:30:43 +0100

    Menu Started

--- Game.js
+++ Game.js
@@ -15,6 +15,11 @@ ctx = canvas.getContext("2d");
 var test;
 var touchXPos, touchYPos;
 var coin;
+var backgroundTex;
+
+var bMenu = true;
+var bPlay = false;
+var bOptions = false;
 
 function Game(){
 	this.init();
@@ -35,24 +40,40 @@ Game.prototype.init=function()
 	coin.src = 'Images/coin.png'; // Set source path
 	}
 
+	backgroundTex = new Image();
+	backgroundTex.addEventListener("load", function()
+	{
+
+	}, false);
+	backgroundTex.src = 'Images/background.png';
+
 Game.prototype.update=function()
 {
 	console.log('Updating');
 
-	// first animated sprite
-	if (Date.now() - oldTime > 1000 / fps)
-	{
-		if (imageFrame == 9)
-		{
-			imageFrame = 0;
-		}
-
-		imageFrame++;
-
-		oldTime = Date.now();
-		ctx.clearRect(20,0,44,44);
-		ctx.drawImage(coin,imageFrame*44, 0,44, 44, 20,0,44,44 );
+	if (bMenu === true){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.drawImage(backgroundTex, 0, 0, canvas.width, canvas.height);
 	}
+	
+	if (bPlay === true){
+		// first animated sprite
+		if (Date.now() - oldTime > 1000 / fps)
+		{
+			if (imageFrame == 9)
+			{
+				imageFrame = 0;
+			}
+
+			imageFrame++;
+
+			oldTime = Date.now();
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			ctx.clearRect(20,0,44,44);
+			ctx.drawImage(coin,imageFrame*44, 0,44, 44, 20,0,44,44 );
+		}
+	}
+	
 
 	window.requestAnimationFrame(myGame.update);
 }
@@ -69,4 +90,22 @@ function onTouchStart(e)
     touchYPos = e.touches[0].clientY;
 
     console.log(touchXPos, touchYPos);
+
+    if (bMenu === true)
+    {
+    	if (touches[0].clientX >= 25 &&
+	    	touches[0].clientX <= 360 &&
+	    	touches[0].clientY >= 0 &&
+	    	touches[0].clientY <= 640)
+	    {
+	    	console.log("Play Button Pressed");
+	    	bMenu = false;
+	    	bPlay = true;
+	    }
+    }
+
+    if (bPlay === true)
+    {
+
+    }
 }
